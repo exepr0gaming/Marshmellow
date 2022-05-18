30cbafec399378ad1e5edbdfbf905f5d97587135
AndrewKurdin <exehomework@gmail.com>
2022-05-18 15:40:16 +0300

    Initial Commit

---
+++ Marshmellow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marshmellow
+//
+//  Created by Курдин Андрей on 18.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marshmellow/MarshmellowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarshmellowApp.swift
+//  Marshmellow
+//
+//  Created by Курдин Андрей on 18.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MarshmellowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
